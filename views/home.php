27a7c8423a13a267c0b341cb81e879188b48eb13
karlaem <60242444+karlaem@users.noreply.github.com>
2020-11-09 01:59:21 -0800

    edited responsive

--- views/home.php
+++ views/home.php
@@ -4,6 +4,7 @@
     <div class="herocontent">
         <h2>Keep What is meaningful</h2>
         <h1>Hand made custom Patches</h1>
+        <p>Beautiful patches for your outfits and bags know more information by registering below:</p>
         <a href="#" class="ctabtn">Register</a>
     </div>
     </div>
